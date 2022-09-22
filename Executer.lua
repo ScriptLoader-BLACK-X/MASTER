68794c97001a9fd10fbd756e90a267cb2b4718ee
BLACK-X HUB <112774539+ScriptLoader-BLACK-X@users.noreply.github.com>
2022-09-22 14:32:08 +0300

    Update Executer.lua

--- Executer.lua
+++ Executer.lua
@@ -1,1 +1,12 @@
 
+
+
+
+
+--DOORS
+if game.PlaceId == 6839171747 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/DOORS-GAME", true))()
+end
+if game.PlaceId == 6516141723 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/DOORS-LOBBY", true))()
+end
