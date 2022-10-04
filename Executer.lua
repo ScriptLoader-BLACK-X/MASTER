3807ff494f502bbc910d0b66f911d8c41df3cb71
BLACK-X HUB <112774539+ScriptLoader-BLACK-X@users.noreply.github.com>
2022-10-04 18:27:07 +0300

    Update Executer.lua

--- Executer.lua
+++ Executer.lua
@@ -1,7 +1,12 @@
 if game.PlaceId == 6516141723 or 6839171747 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/DOORS", true))()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/DOORS", true))()
 end
-if game.PlaceId == 00 then
+if game.PlaceId == 9498006165 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/TappingSimulator", true))()
 end
-if game.PlaceId == 00 then
+if game.PlaceId == 8612195438 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/ClickerPartySimulator", true))()
+end
+if game.PlaceId == 9498006165 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/TappingSimulator", true))()
 end
