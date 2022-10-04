1b7b43a51d1396e097c6bfbc72b7b87221fe6384
BLACK-X HUB <112774539+ScriptLoader-BLACK-X@users.noreply.github.com>
2022-10-04 21:34:23 +0300

    Update Executer.lua

--- Executer.lua
+++ Executer.lua
@@ -1,12 +1,41 @@
-if game.PlaceId == 6516141723 or 6839171747 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/DOORS", true))()
-end
-if game.PlaceId == 9498006165 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/TappingSimulator", true))()
-end
-if game.PlaceId == 8612195438 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/ClickerPartySimulator", true))()
-end
-if game.PlaceId == 9498006165 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/TappingSimulator", true))()
-end
+
+
+
+
+
+
+local Games = {
+    [6516141723] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/DOORS",
+    [6839171747] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/DOORS",
+    [8612195438] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/ClickerPartySimulator",
+    [9498006165] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/TappingSimulator",
+    [9992339729] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/LongestAnswerWin",
+}
+
+
+
+
+if Games[game.PlaceId] == nil then
+    CoreGui:SetCore("SendNotification", {
+        Title = "Game not supported :(";
+        Text = "Copying discord to clipboard... Please check the game support channel for more information.";
+        Duration = 3;
+    })
+    
+    task.delay(2, function()
+        setclipboard("https://discord.gg/k9a4zym5uG")
+            CoreGui:SetCore("SendNotification", {
+            Title = "Loading";
+            Text = "Game was not supported so its now loading Hydra Network Universal.";
+            Duration = 3;
+        })
+        loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/Universal.lua"), true))()
+    end)
+else
+    CoreGui:SetCore("SendNotification", {
+        Title = "Discord";
+        Text = "We would appreciate if you joined our discord (Copied)";
+        Duration = 10;
+    })
+    setclipboard("https://discord.gg/k9a4zym5uG")
+    loadstring(game:HttpGet(GameTable[game.PlaceId]))()
