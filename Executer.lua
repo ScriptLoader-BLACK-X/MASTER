83da5607245a574c91430c3e85dae94d2cd2eab6
BLACK-X HUB <112774539+ScriptLoader-BLACK-X@users.noreply.github.com>
2022-10-04 22:03:12 +0300

    Update Executer.lua

--- Executer.lua
+++ Executer.lua
@@ -1,10 +1,22 @@
-local Games = {
-    [6516141723] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/DOORS",
-    [6839171747] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/DOORS",
-    [8612195438] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/ClickerPartySimulator",
-    [9498006165] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/TappingSimulator",
-    [9992339729] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/LongestAnswerWin",
+
+
+local GameTable = {
+    [6516141723] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/DOORS";
+    [6839171747] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/DOORS";
+    [8612195438] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/ClickerPartySimulator";
+    [9498006165] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/TappingSimulator";
+    [9992339729] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/LongestAnswerWin"
 }
-if Games[game.PlaceId] == nil then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/BLACK-X-HUB-INTRO", true))()
-loadstring(game:HttpGet(Games[game.PlaceId]))()
+
+
+
+
+if GameTable[game.PlaceId] == nil then
+    
+    task.delay(2, function()
+        loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/Universal.lua"), true))()
+    end)
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/BLACK-X-HUB-INTRO", true))()
+    loadstring(game:HttpGet(GameTable[game.PlaceId]))()
+end
