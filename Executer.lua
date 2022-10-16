66285893216f7357ef4c77db3d39b8086f140b83
BLACK-X HUB <112774539+ScriptLoader-BLACK-X@users.noreply.github.com>
2022-10-16 15:34:58 +0300

    Update Executer.lua

--- Executer.lua
+++ Executer.lua
@@ -1,1 +1,10 @@
+local GameTable = {
+    [9992339729] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/LongestAnswerWin",
+}
 
+if GameTable[game.PlaceId] == nil then
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/DOSENT%20SUPPORTED%20GAME')))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/BLACK-X-HUB-INTRO"))()
+    loadstring(game:HttpGet(GameTable[game.PlaceId]))()
+end
