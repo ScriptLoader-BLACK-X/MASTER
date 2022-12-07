6277de50295dc04d4e48dd853f4b614b37f13582
BLACK-X HUB <112774539+ScriptLoader-BLACK-X@users.noreply.github.com>
2022-12-07 05:23:49 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,11 +1,12 @@
 local GameTable = {
-    [6516141723] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/Doors",
-    [6839171747] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/Doors",
-    [9498006165] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/TappingSilmulator"
+    [6516141723] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/MASTER/main/Main.lua",
+    [6839171747] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/MASTER/main/Main.lua",
+    [9498006165] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/MASTER/main/Main.lua",
+    [7562033213] = "https://raw.githubusercontent.com/ScriptLoader-BLACK-X/MASTER/main/Main.lua"
 }
 if GameTable[game.PlaceId] == nil then
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/DOSENT%20SUPPORTED%20GAME')))()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/ScriptLoader-BLACK-X/MASTER/main/GAME-DOSENT-SUPPORTED')))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptLoader-BLACK-X/GAMES/main/BLACK-X-HUB-INTRO"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptLoader-BLACK-X/MASTER/main/BLACK-X-HUB-INTRO"))()
     loadstring(game:HttpGet(GameTable[game.PlaceId]))()
 end
